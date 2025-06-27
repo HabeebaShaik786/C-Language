df9c4e024048b7e2e51432a5c381d9c93fa6d0da
shaik habeeba <shabeeba713@gmail.com>
2025-06-27 10:22:37 +0530

    Create No Arg No return func.c

---
+++ No Arg No return func.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void hi()
+{
+
+    printf("good morning students!!!!");
+}
+int main()
+{
+    hi();
+    return 0;
+}
+
+#include<stdio.h>
+void number()
+{
+    int num;
+    printf("enter a number:");
+    scanf("%d",&num);
+    printf("you have entered %d \n",num);
+}
+int main()
+{
+    number();
+    return 0;
+}
